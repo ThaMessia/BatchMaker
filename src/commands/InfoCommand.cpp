c9367a26ac2fd1af3b6106fbe7f901dfeb0d259b
LoRy24 <lorytherock24tutorial@gmail.com>
2021-03-26 22:49:48 +0100

    Update InfoCommand.cpp

--- src/commands/InfoCommand.cpp
+++ src/commands/InfoCommand.cpp
@@ -11,9 +11,9 @@ void executeInfo() {
 	cout << "---" << endl;
 	setcolor(0x4);
 	cout << "BatchMaker e' un software ideato per la realizzazione di malware in Batch.\nIl progetto e' stato inizialmente sviluppato dal team di Sicurezza Informatica" << endl;
-    cout << "Sapphire Security, che ha passato la cura del progetto alla comunità \"EnJava\". Il programma permette la creazione di " << 
+    cout << "Sapphire Security, che ha passato la cura del progetto alla comunita' \"EnJava\". Il programma permette la creazione di " << endl << 
             "malware altamente pericolosi scritti in Batch.\nLo sviluppatore principale e' ThaMessia" << endl;
     setcolor(0x7);
     cout << "---" << endl;
-    setcolor(4);
+    setcolor(0x4);
 }
