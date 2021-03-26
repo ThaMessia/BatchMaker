cd5635e5c0574c15d51fb215bde43f40f53ef5cc
LoRy24 <lorytherock24tutorial@gmail.com>
2021-03-26 21:31:09 +0100

    Update ListCommand.cpp

--- src/commands/ListCommand.cpp
+++ src/commands/ListCommand.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 void executeListCmd() {
-    writeTextSquared("SimpleMalware", 7, 4);
-	writeTextSquared("PcDown", 7, 4);
-	writeTextSquared("RipComputer", 7, 4);
-	writeTextSquared("EnJavaError", 7, 4);
-	writeTextSquared("TookMouse", 7, 4);
-	writeTextSquared("BlockConnection", 7, 4);
-	writeTextSquared("PcDestroy", 7, 4);
-	writeTextSquared("SilentMalware", 7, 4);
+    writeTextSquared("SimpleMalware", 0x7, 0x4);
+	writeTextSquared("PcDown", 0x7, 0x4);
+	writeTextSquared("RipComputer", 0x7, 0x4);
+	writeTextSquared("EnJavaError", 0x7, 0x4);
+	writeTextSquared("TookMouse", 0x7, 0x4);
+	writeTextSquared("BlockConnection", 0x7, 0x4);
+	writeTextSquared("PcDestroy", 0x7, 0x4);
+	writeTextSquared("SilentMalware", 0x7, 0x4);
+	setcolor(0x4);
 }
