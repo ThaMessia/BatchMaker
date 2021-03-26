4cde19123e59c7b1f672ffd40d0d9d8bfc370591
LoRy24 <lorytherock24tutorial@gmail.com>
2021-03-26 21:16:16 +0100

    Created the ListCommand

---
+++ src/commands/ListCommand.cpp
@@ -0,0 +1,14 @@
+#include "../ConsoleUtils.h"
+
+using namespace std;
+
+void executeListCmd() {
+    writeTextSquared("SimpleMalware", 7, 4);
+	writeTextSquared("PcDown", 7, 4);
+	writeTextSquared("RipComputer", 7, 4);
+	writeTextSquared("EnJavaError", 7, 4);
+	writeTextSquared("TookMouse", 7, 4);
+	writeTextSquared("BlockConnection", 7, 4);
+	writeTextSquared("PcDestroy", 7, 4);
+	writeTextSquared("SilentMalware", 7, 4);
+}
